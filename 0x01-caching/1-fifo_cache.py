e4778267180343e4f1a55a20665f394776e5fb6a
EnGentech <engen.inyang@gmail.com>
2023-10-24 20:05:50 +0100

    modified task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -13,15 +13,16 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """insert into cache_data __dict__"""
-        cache_data_count = len(self.cache_data)
+        if key and item:
+            cache_data_count = len(self.cache_data)
 
-        if cache_data_count >= self.MAX_ITEMS and key not in self.cache_data:
-            firstItem = next(iter(self.cache_data.keys()))
-            print(f"Discard: {firstItem}")
-            self.cache_data.pop(firstItem)
-            self.cache_data[key] = item
-        else:
-            self.cache_data[key] = item
+            if cache_data_count >= self.MAX_ITEMS and key not in self.cache_data:
+                firstItem = next(iter(self.cache_data.keys()))
+                print(f"Discard: {firstItem}")
+                self.cache_data.pop(firstItem)
+                self.cache_data[key] = item
+            else:
+                self.cache_data[key] = item
 
     def get(self, key):
         """retrieve item from storage"""
