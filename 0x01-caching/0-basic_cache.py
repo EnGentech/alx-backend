b9b08a8a6a2373995aac57d86de376ab8a052375
EnGentech <engen.inyang@gmail.com>
2023-10-24 19:49:05 +0100

    fixed bugs in task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,13 +11,11 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """insert into cache_data __dict__"""
-        self.cache_data[key] = item
+        if key and item:
+            self.cache_data[key] = item
 
     def get(self, key):
         """retrieve item from storage"""
-        if key in self.cache_data:
-            return self.cache_data.get(key)
-        else:
-            return None
+        return self.cache_data.get(key, None)
 
 # Coded by EnGentech
