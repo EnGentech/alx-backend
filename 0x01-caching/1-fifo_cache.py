bd8ee5e05c056136645d60a115a0353d11f19ae4
EnGentech <engen.inyang@gmail.com>
2023-10-24 22:48:29 +0100

    still working on it

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -16,7 +16,8 @@ class FIFOCache(BaseCaching):
         if key and item:
             cache_data_count = len(self.cache_data)
 
-            if cache_data_count >= self.MAX_ITEMS and key not in self.cache_data:
+            if cache_data_count >= self.MAX_ITEMS \
+                    and key not in self.cache_data:
                 firstItem = next(iter(self.cache_data.keys()))
                 print(f"Discard: {firstItem}")
                 self.cache_data.pop(firstItem)
