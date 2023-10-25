edea89eeafd9df4db32a84f9097bf5fd23fa07b7
EnGentech <engen.inyang@gmail.com>
2023-10-25 15:50:53 +0100

    first attempt

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""MRU caching"""
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LFUCache(BaseCaching):
+    """class definition"""
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+        self.track = []
+
+    def put(self, key, item):
+        """insert into cache_data __dict__"""
+        if key and item:
+            if key not in self.cache_data:
+                if len(self.cache_data) >= self.MAX_ITEMS:
+                    firstKey, _ = self.cache_data.popitem(False)
+                    print("DISCARD:", firstKey)
+                self.cache_data[key] = item
+                self.cache_data.move_to_end(key, last=False)
+            else:
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """retrieve item from storage"""
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+            return self.cache_data.get(key)
+        else:
+            return None
+
+# Coded by EnGentech
