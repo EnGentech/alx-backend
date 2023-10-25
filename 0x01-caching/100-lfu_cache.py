9c0c8f82ccc1aaee09daa186caf7e4585ca01bc0
EnGentech <engen.inyang@gmail.com>
2023-10-25 23:42:10 +0100

    implemented Least Frequently used with the help of big team

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,35 +1,58 @@
 #!/usr/bin/python3
-"""MRU caching"""
+""" LFU Cache module
+"""
 
 from base_caching import BaseCaching
-from collections import OrderedDict
 
 
 class LFUCache(BaseCaching):
-    """class definition"""
+    """ LFU caching system
+    """
+
     def __init__(self):
+        """ Initialize LFUCache
+        """
         super().__init__()
-        self.cache_data = OrderedDict()
-        self.track = []
+        self.keys_usage = {}
+        self.usage_keys = {}
+        self.counter = 0
+        self.min_usage = 1  # Initialize min_usage
 
     def put(self, key, item):
-        """insert into cache_data __dict__"""
-        if key and item:
-            if key not in self.cache_data:
-                if len(self.cache_data) >= self.MAX_ITEMS:
-                    firstKey, _ = self.cache_data.popitem(False)
-                    print("DISCARD:", firstKey)
-                self.cache_data[key] = item
-                self.cache_data.move_to_end(key, last=False)
-            else:
-                self.cache_data[key] = item
+        """ Add an item to the cache
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                while self.min_usage not in self.usage_keys or\
+                        not self.usage_keys[self.min_usage]:
+                    self.min_usage += 1
+
+                min_usage_keys = self.usage_keys[self.min_usage]
+                lru_key = min(min_usage_keys, key=lambda k: min_usage_keys[k])
+                print(f"DISCARD: {lru_key}")
+                del self.cache_data[lru_key]
+                del self.keys_usage[lru_key]
+                del self.usage_keys[self.min_usage][lru_key]
+
+            self.cache_data[key] = item
+            self.keys_usage[key] = 1
+            self.usage_keys.setdefault(1, {})[key] = self.counter
+            self.counter += 1
+            self.min_usage = 1  # Reset min_usage after adding a new item
 
     def get(self, key):
-        """retrieve item from storage"""
-        if key in self.cache_data:
-            self.cache_data.move_to_end(key, last=False)
-            return self.cache_data.get(key)
-        else:
-            return None
+        """ Get an item from the cache
+        """
+        if key is not None and key in self.cache_data:
+            self.keys_usage[key] += 1
+            usage = self.keys_usage[key]
+            old_usage = usage - 1
+            del self.usage_keys[old_usage][key]
+            if old_usage not in self.usage_keys:
+                self.min_usage = old_usage + 1
+            self.usage_keys.setdefault(usage, {})[key] = self.counter
+            self.counter += 1
+            return self.cache_data[key]
+        return None
 
-# Coded by EnGentech
+# BigTeam support
