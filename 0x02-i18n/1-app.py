3e2066217dacd970721285ff919552e943679c98
EnGentech <engen.inyang@gmail.com>
2023-10-31 20:55:50 +0100

    configured babel file

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Basic flask config
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """class definition"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    """
+    A function for a basic route
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
